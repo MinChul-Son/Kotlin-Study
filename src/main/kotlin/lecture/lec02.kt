befdf65db602fabdfd46e0e07d1fd2d3b2384cfc
MinChul-Son <smc5236@gmail.com>
2022-06-22 16:25:02 +0900

    🌱 feat: null 실습 - 코틀린스럽게 변경

--- src/main/kotlin/lecture/lec02.kt
+++ src/main/kotlin/lecture/lec02.kt
@@ -14,17 +14,30 @@ fun main() {
 }
 
 fun startsWithA1(str: String?): Boolean {
-    if (str == null) {
-        throw IllegalArgumentException("null은 들어올 수 없어요!")
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: throw IllegalArgumentException("null은 들어올 수 없어요!")
+
+//    if (str == null) {
+//        throw IllegalArgumentException("null은 들어올 수 없어요!")
+//    }
+//    return str.startsWith("A")
 }
 
 fun startsWithA2(str: String?): Boolean? {
-    if (str == null) {
-        return null
-    }
-    return str.startsWith("A")
+    return str?.startsWith("A")
+
+//    if (str == null) {
+//        return null
+//    }
+//    return str.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+    return str?.startsWith("A") ?: false
+
+//    if (str == null) {
+//        return false
+//    }
+//    return str.startsWith("A")
 }
 
 
